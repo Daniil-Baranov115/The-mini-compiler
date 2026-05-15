765542237a928f2d3d6dca453608c0d22fa3dd5b
Dan4ik <Tylov15AA@gmail.com>
2026-05-15 09:52:41 +0300

    the_final_version?

---
+++ build/CMakeFiles/mini_compiler.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mini_compiler.dir/src/ast.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/ast.cpp.o.d"
+  "CMakeFiles/mini_compiler.dir/src/driver.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/driver.cpp.o.d"
+  "CMakeFiles/mini_compiler.dir/src/lexer.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/lexer.cpp.o.d"
+  "CMakeFiles/mini_compiler.dir/src/llvm_codegen.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/llvm_codegen.cpp.o.d"
+  "CMakeFiles/mini_compiler.dir/src/main.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/main.cpp.o.d"
+  "CMakeFiles/mini_compiler.dir/src/parser.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/parser.cpp.o.d"
+  "CMakeFiles/mini_compiler.dir/src/sema.cpp.o"
+  "CMakeFiles/mini_compiler.dir/src/sema.cpp.o.d"
+  "mini_compiler"
+  "mini_compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mini_compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
